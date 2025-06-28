563ad01035481a702016dcc44e2f9c3df6709998
bowman <bowmanonjupiter2@gmail.com>
2025-06-28 21:37:27 +0800

    added reverse op

--- pythonProject/SparseArray/sparse-array.py
+++ pythonProject/SparseArray/sparse-array.py
@@ -25,13 +25,29 @@ def file_to_sparse_array(file_path):
 
     result = [[0 for _ in range(collection[0][1])] for _ in range(collection[0][0])]
 
-# skip the first element in collection as it contains the dimensions
+    # skip the first element in collection as it contains the dimensions
     for row, col, value in collection[1:]:
         if row >= len(result) or col >= len(result[0]):
             print(f"Error: Index out of bounds for row {row}, column {col}.")
             sys.exit(1)
         result[row][col] = value
 
+    return result
+
+
+def sparse_array_to_file(sparse_array):
+    if sparse_array is None:
+        print(f'Error: Sparse array is None.')
+        sys.exit(1)
+    result = []
+
+    non_zero = 0
+    for i in range(len(sparse_array)):
+        for j in range(len(sparse_array[0])):
+            if sparse_array[i][j] != 0:
+                result.append([i, j, sparse_array[i][j]])
+                non_zero += 1
+    result.insert(0, [len(sparse_array), len(sparse_array[0]), non_zero])
     return result
 
 
@@ -42,6 +58,12 @@ def main():
         print("Sparse Array:")
         for row in result:
             print(' '.join(map(str, row)))
+
+        sparse_result = sparse_array_to_file(result)
+        if sparse_result:
+            print("\nSparse Array Representation:")
+            for row in sparse_result:
+                print(', '.join(map(str, row)))
 
 
 if __name__ == "__main__":
